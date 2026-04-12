13f21d649ff2a56c43bdc8ba41fdad7c8c771597
AbbasSavvy <abbasteen@gmail.com>
2026-04-12 14:57:11 +0530

    adding src folder

---
+++ src/loader.py
@@ -0,0 +1,15 @@
+from pypdf import PdfReader
+
+def load_pdf(path):
+    reader = PdfReader(path)
+    pages = []
+    for i, page in enumerate(reader.pages):
+        text = page.extract_text()
+        if text:
+            pages.append({
+                "page": i + 1,
+                "text": text,
+                "source": path
+            })
+    return pages
+
